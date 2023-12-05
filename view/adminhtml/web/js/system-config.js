61d265344ef883698147a090ede40b4057ede509
sam <sam@kammalou.com>
2023-12-05 17:17:33 +0530

    feat: worked on the category selection scrolling, message improvment, naming etc..

--- view/adminhtml/web/js/system-config.js
+++ view/adminhtml/web/js/system-config.js
@@ -2,10 +2,11 @@ require(["jquery"], function ($) {
     $(document).ready(function () {
         function toggleClientKeyField() {
             var paymentMethodField = $("#sales_channel_general_payment_method").val();
-            var clientKeyField = $("#row_sales_channel_general_payment_method_client_key");
+            var clientKeyField = $(
+                "#row_sales_channel_general_payment_method_client_key"
+            );
             if (paymentMethodField && paymentMethodField.length > 1) {
                 clientKeyField.show();
-                console.log("show");
             } else {
                 clientKeyField.hide();
             }
@@ -15,5 +16,19 @@ require(["jquery"], function ($) {
         $("#sales_channel_general_payment_method").change(function () {
             toggleClientKeyField();
         });
+
+        // Scroll to selected category
+
+        var multiselectField = $('#sales_channel_general_catalog_id');
+        var selectedOption = multiselectField.find('option:selected');
+
+        if (selectedOption.length > 0) {
+            var multiselectHeight     = multiselectField.height();
+            var selectedOptionOffset  = selectedOption.position().top;
+            var halfMultiselectHeight = multiselectHeight / 2;
+            var scrollToCenter        = selectedOptionOffset - halfMultiselectHeight;
+
+            multiselectField.scrollTop(scrollToCenter);
+        }
     });
 });
